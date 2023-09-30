0965465a699902a0289be61ed63e691d89d4a914
Ramill <ram4ik@hotmail.com>
2023-09-30 07:43:27 +0300

    Initial Commit

---
+++ DesignPatternVGT/DesignPatternVGTApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatternVGTApp.swift
+//  DesignPatternVGT
+//
+//  Created by admin on 30.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct DesignPatternVGTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
